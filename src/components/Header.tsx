ffb3c75a8cc72fc6bb5dd7dd532d7866d89bec54
gurhanciftci <outlawyer@yandex.com>
2025-06-29 21:40:37 +0300

    Fix HulyNewsLogo import in Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useTheme } from '../contexts/ThemeContext';
-import HulyNewsLogo from './HulyNewsLogo';
+import HulyNewsLogo from './HulyNewsLogo.tsx';
 
 interface HeaderProps {
   onPageChange?: (page: 'news' | 'favorites') => void;
@@ -51,7 +51,7 @@ export default function Header({ onPageChange, currentPage = 'news', onOpenPrefe
                 }`}
               >
                 <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z" />
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 01-2-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z" />
                 </svg>
                 Haberler
               </button>
